f65640ed9b60f0782b0c16697a38ed9e218fcf69
Diego Tiozzo Fasiolo <diego.tiozzo@uniud.it>
2024-03-13 17:09:24 -0400

    Fix

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -10,7 +10,7 @@
 
 #include "cloud_msgs/cloud_info.h"
 
-#include <opencv/cv.h>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -50,7 +50,7 @@ using namespace std;
 
 typedef pcl::PointXYZI  PointType;
 
-extern const string pointCloudTopic = "/velodyne_points";
+extern const string pointCloudTopic = "/ouster/points";
 extern const string imuTopic = "/imu/data";
 
 // Save pcd
@@ -60,12 +60,12 @@ extern const string fileDirectory = "/tmp/";
 extern const bool useCloudRing = true; // if true, ang_res_y and ang_bottom are not used
 
 // VLP-16
-extern const int N_SCAN = 16;
-extern const int Horizon_SCAN = 1800;
-extern const float ang_res_x = 0.2;
-extern const float ang_res_y = 2.0;
-extern const float ang_bottom = 15.0+0.1;
-extern const int groundScanInd = 7;
+//extern const int N_SCAN = 16;
+//extern const int Horizon_SCAN = 1800;
+//extern const float ang_res_x = 0.2;
+//extern const float ang_res_y = 2.0;
+//extern const float ang_bottom = 15.0+0.1;
+//extern const int groundScanInd = 7;
 
 // HDL-32E
 // extern const int N_SCAN = 32;
@@ -83,7 +83,7 @@ extern const int groundScanInd = 7;
 // extern const float ang_bottom = 25.0;
 // extern const int groundScanInd = 10;
 
-// Ouster users may need to uncomment line 159 in imageProjection.cpp
+// Ouster users may need to uncomment line 166 in imageProjection.cpp
 // Usage of Ouster imu data is not fully supported yet (LeGO-LOAM needs 9-DOF IMU), please just publish point cloud data
 // Ouster OS1-16
 // extern const int N_SCAN = 16;
@@ -94,14 +94,14 @@ extern const int groundScanInd = 7;
 // extern const int groundScanInd = 7;
 
 // Ouster OS1-64
-// extern const int N_SCAN = 64;
-// extern const int Horizon_SCAN = 1024;
-// extern const float ang_res_x = 360.0/float(Horizon_SCAN);
-// extern const float ang_res_y = 33.2/float(N_SCAN-1);
-// extern const float ang_bottom = 16.6+0.1;
-// extern const int groundScanInd = 15;
+extern const int N_SCAN = 64;
+extern const int Horizon_SCAN = 1024;
+extern const float ang_res_x = 360.0/float(Horizon_SCAN);
+extern const float ang_res_y = 33.2/float(N_SCAN-1);
+extern const float ang_bottom = 16.6+0.1;
+extern const int groundScanInd = 15;
 
-extern const bool loopClosureEnableFlag = false;
+extern const bool loopClosureEnableFlag = true;
 extern const double mappingProcessInterval = 0.3;
 
 extern const float scanPeriod = 0.1;
